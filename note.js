2776a095835a913885ea4c34e60efa34273daa03
fathan-nurudin <fathan011197@gmail.com>
2020-11-20 15:37:37 +0700

    latihan-project

---
+++ note.js
@@ -0,0 +1,20 @@
+let addWordButton = document.getElementById('addWord')
+let input = document.getElementById('inputWord')
+let container = document.getElementById('wordContainer')
+
+if(input === '') {
+    alert('Please write something.')
+}
+// addWordButton.addEventListener('click', function() {
+//     let paragraph = document.createElement('p')
+//     paragraph.classList.add('paragraph-styling')
+//     paragraph.innerText = input.value;
+//     container.appendChild(paragraph)
+//     input.value = ""
+//     paragraph.addEventListener('click', function(){
+//         paragraph.style.textDecoration = "underline"
+//     })
+//     paragraph.addEventListener('dblclick', function(){
+//         container.removeChild(paragraph)
+//     })
+// })
