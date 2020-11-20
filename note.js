98b8e563a61e86674dcc0eb61f8df47040664200
fathan-nurudin <fathan011197@gmail.com>
2020-11-20 15:49:27 +0700

    fathan

--- note.js
+++ note.js
@@ -5,16 +5,16 @@ let container = document.getElementById('wordContainer')
 if(input === '') {
     alert('Please write something.')
 }
-// addWordButton.addEventListener('click', function() {
-//     let paragraph = document.createElement('p')
-//     paragraph.classList.add('paragraph-styling')
-//     paragraph.innerText = input.value;
-//     container.appendChild(paragraph)
-//     input.value = ""
-//     paragraph.addEventListener('click', function(){
-//         paragraph.style.textDecoration = "underline"
-//     })
-//     paragraph.addEventListener('dblclick', function(){
-//         container.removeChild(paragraph)
-//     })
-// })
+addWordButton.addEventListener('click', function() {
+    let paragraph = document.createElement('p')
+    paragraph.classList.add('paragraph-styling')
+    paragraph.innerText = input.value;
+    container.appendChild(paragraph)
+    input.value = ""
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "underline"
+    })
+    paragraph.addEventListener('dblclick', function(){
+        container.removeChild(paragraph)
+    })
+})
